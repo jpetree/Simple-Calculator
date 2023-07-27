d18d3344380ca451bd2eb3f21d53c1a6dd3f6ce8
Josh Petree <35755661+jpetree@users.noreply.github.com>
2023-07-27 10:01:41 -0500

    Add files via upload

---
+++ simpleCalc.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Net.Security
+
+Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim iFirst As Integer
+        Dim iSecond As Integer
+        Dim iResult As Integer
+
+        iFirst = CInt(txtFirst.Text)
+        iSecond = CInt(txtSecond.Text)
+
+        If listOp.SelectedItem = "+" Then
+            iResult = iFirst + iSecond
+        ElseIf listOp.SelectedItem = "-" Then
+            iResult = iFirst - iSecond
+        ElseIf listOp.SelectedItem = "*" Then
+            iResult = iFirst * iSecond
+        ElseIf listOp.SelectedItem = "/" Then
+            iResult = iFirst / iSecond
+        End If
+
+        MsgBox(iFirst & listOp.SelectedItem & iSecond & " is equal to: " & iResult)
+
+    End Sub
+End Class
